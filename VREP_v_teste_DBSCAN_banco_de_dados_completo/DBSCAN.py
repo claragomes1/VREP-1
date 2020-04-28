017a4094918ded593f5bd54554556330b8704601
joao <zenobiojoao@gmail.com>
2020-04-28 16:36:52 -0300

    * utilizando dados com quantidades parecidas * utilizando normalizacao (0-1) * utilizando avaliacao com score (adjusted_rand_score)

--- VREP_v_teste_DBSCAN_banco_de_dados_completo/DBSCAN.py
+++ VREP_v_teste_DBSCAN_banco_de_dados_completo/DBSCAN.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.cluster import DBSCAN
 import plotly.express as px
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics.cluster import adjusted_rand_score
 
 corredorDf = pd.read_csv('corredor.csv', index_col='object')
 saida_direitaDf = pd.read_csv('saida_direita.csv', index_col='object')
@@ -12,9 +14,14 @@ dfComplet = saida_direitaDf.append(corredorDf, ignore_index=True)
 dfComplet.dropna(inplace=True)
 
 dfCompletNoLabel = dfComplet.drop(columns=['label'])
-dbscan = DBSCAN(eps=0.086, metric='correlation').fit(dfCompletNoLabel)       # eps=0.00928 cosine / eps=0.08640 correlation
+scaler = MinMaxScaler(copy=False)
+scaler.fit(dfCompletNoLabel)
+scaler.transform(dfCompletNoLabel)
+dbscan = DBSCAN(eps=0.12, metric='correlation').fit(dfCompletNoLabel)   # eps=0.017 cosine / eps=0.12 correlation / eps=1.2 euclidean
 dfComplet['cluster'] = dbscan.labels_
 
+score = adjusted_rand_score(dfComplet['label'], dfComplet['cluster'])
+
 dfComplet['object'] = dfComplet.index
-fig = px.scatter(dfComplet, x='object', y="cluster", color=dfComplet['label'])
+fig = px.scatter(dfComplet, x='object', y="cluster", color=dfComplet['label'], title=str(score))
 fig.show()
