aaf41f99641b95516f0b82e448bae26499f00f9b
zenobio77 <zenobiojoao@gmail.com>
2020-07-12 07:58:02 -0300

    dados dtw

---
+++ webots/joao/controllers/pioneer_360/corredor_maior/pioneer_collecting.py
@@ -0,0 +1,34 @@
+from controller import Robot
+import pandas as pd
+from csv import writer
+
+def appendListAsRow(file_name, list_of_elem):
+    with open(file_name, 'a+', newline='') as write_obj:
+        csv_writer = writer(write_obj)
+        csv_writer.writerow(list_of_elem)
+
+TIME_STEP = 32
+robot = Robot()
+lidar = robot.getLidar("LDS-01")
+lidar.enable(TIME_STEP)
+lidar.enablePointCloud()
+left_wheel = robot.getMotor("left wheel")
+left_wheel.setPosition(float('inf'))
+left_wheel.setVelocity(3.0)
+right_wheel = robot.getMotor("right wheel")
+right_wheel.setPosition(float('inf'))
+right_wheel.setVelocity(3.0)
+
+robot.step(TIME_STEP)
+robot.step(TIME_STEP)
+range_image = lidar.getRangeImage()
+range_image_df = pd.DataFrame([range_image])
+range_image_df.to_csv('corredor_variado.csv')
+
+counter = 1
+lado = 90
+corredor = range_image[lado]
+while robot.step(TIME_STEP) != -1:
+    range_image = lidar.getRangeImage()
+    appendListAsRow('corredor_variado.csv', [counter] + range_image)
+    counter += 1
--- webots/joao/controllers/pioneer_360/dbscan.py
+++ webots/joao/controllers/pioneer_360/dbscan.py
@@ -1,118 +1,103 @@
 import math
-import statistics
+from functools import wraps
+from time import time
 import pandas as pd
 import numpy as np
 from sklearn.cluster import DBSCAN
-from sklearn.decomposition import PCA
-from sklearn.cluster import OPTICS
-from sklearn.metrics.cluster import adjusted_rand_score
+from dtw import *
 import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 
-# -------------------------------------------------------------------------------------------------------------------------
 
-# Carrega dados
+def timer(function):
+    @wraps(function)
+    def _time_it(*args, **kwargs):
+        start = int(round(time() * 1000))
+        try:
+            return function(*args, **kwargs)
+        finally:
+            end_ = int(round(time() * 1000)) - start
+            name = function.__name__
+            print(f"Total execution time of {name} was: {end_ if end_ > 0 else 0} ms")
+    return _time_it
 
-# corredor_df = pd.read_csv('corredor.csv', index_col=0)
 
-# -------------------------------------------------------------------------------------------------------------------------
+@timer
+def get_data():
+    return pd.read_csv('corredor_maior/corredor.csv', index_col=0)
 
-# DBSCAN 1 e 2 com organização dos clusters de forma antihorária + preparo da animação
 
-# x_complete = []
-# y_complete = []
-# id_complete = []
-# id = -1
-# labels_complete = []
-# for line in corredor_df.values:
-#     id += 1
-#     x = []
-#     y = []
-#     for i in range(len(line)):
-#         x.append(line[i] * math.cos(math.radians(i - 90)))
-#         y.append(line[i] * math.sin(math.radians(i - 90)))
-#     x_y_df = pd.DataFrame(data={'x': x, 'y': y})
-#     dbscan = DBSCAN(eps=0.35, metric='euclidean').fit(x_y_df)
-#     labels_temp = [-1] * len(dbscan.labels_)
-#     label_counter = 0
-#     visited = 0
-#     while visited < len(dbscan.labels_):
-#         cluster = dbscan.labels_[np.where(dbscan.labels_ != -2)[0][0]]
-#         index_list = np.where(dbscan.labels_ == cluster)[0]
-#         for index in index_list:
-#             if cluster != -1:
-#                 labels_temp[index] = label_counter
-#             dbscan.labels_[index] = -2
-#             visited += 1
-#         if cluster != -1:
-#             label_counter += 1
-#     for each in x:
-#         x_complete.append(each)
-#     for each in y:
-#         y_complete.append(each)
-#     for each in [id] * len(line):
-#         id_complete.append(each)
-#     for each in labels_temp:
-#         labels_complete.append(each)
-#
-# distance_complete = []
-# for reading in range(len(corredor_df)):
-#     for degree in corredor_df.values[reading]:
-#         distance_complete.append(degree)
-#
-# topography_df = pd.DataFrame(data={'x': x_complete, 'y': y_complete, 'distance': distance_complete, 'id': id_complete, 'label': labels_complete})
-# topography_df.to_csv('topography_df.csv')
+@timer
+def DBSCAN_stage_1():
+    corredor_df = get_data()
+    x_complete = []
+    y_complete = []
+    id_complete = []
+    id = -1
+    labels_complete = []
+    for line in corredor_df.values:
+        id += 1
+        x = []
+        y = []
+        for i in range(len(line)):
+            x.append(line[i] * math.cos(math.radians(i - 90)))
+            y.append(line[i] * math.sin(math.radians(i - 90)))
+        x_y_df = pd.DataFrame(data={'x': x, 'y': y})
+        dbscan = DBSCAN(eps=0.35, metric='euclidean').fit(x_y_df)
+        labels_temp = [-1] * len(dbscan.labels_)
+        label_counter = 0
+        visited = 0
+        while visited < len(dbscan.labels_):
+            cluster = dbscan.labels_[np.where(dbscan.labels_ != -2)[0][0]]
+            index_list = np.where(dbscan.labels_ == cluster)[0]
+            for index in index_list:
+                if cluster != -1:
+                    labels_temp[index] = label_counter
+                dbscan.labels_[index] = -2
+                visited += 1
+            if cluster != -1:
+                label_counter += 1
+        for each in x:
+            x_complete.append(each)
+        for each in y:
+            y_complete.append(each)
+        for each in [id] * len(line):
+            id_complete.append(each)
+        for each in labels_temp:
+            labels_complete.append(each)
+    distance_complete = []
+    for reading in range(len(corredor_df)):
+        for degree in corredor_df.values[reading]:
+            distance_complete.append(degree)
+    topography_df = pd.DataFrame(data={'x': x_complete, 'y': y_complete, 'distance': distance_complete, 'id': id_complete, 'label': labels_complete})
+    topography_df.to_csv('topography_df.csv')
 
-# -------------------------------------------------------------------------------------------------------------------------
 
-#...
+@timer
+def animation():
+    topography_df = pd.read_csv('topography_df.csv', index_col=0)
+    fig = px.scatter(topography_df, x='x', y='y', color='label', animation_frame='id', range_x=[-5, 5], range_y=[-5, 5])
+    fig.update_yaxes(dtick=0.5)
+    fig.update_xaxes(dtick=0.5)
+    fig.update_layout(height=500, width=500, title_text="Animacao dos pontos no corredor")
+    fig.show()
 
-# -------------------------------------------------------------------------------------------------------------------------
 
-# Gera animação das leituras
+def dtw_metric(x, y):
+    return dtw(x, y, keep_internals=True, step_pattern="symmetric1").distance
 
-# topography_df = pd.read_csv('topography_df.csv', index_col=0)
-# fig = px.scatter(topography_df, x='x', y='y', color='label', animation_frame='id', range_x=[-5, 5], range_y=[-5, 5])
-# fig.update_yaxes(dtick=0.5)
-# fig.update_xaxes(dtick=0.5)
-# fig.update_layout(height=500, width=500, title_text="Animacao dos pontos no corredor")
-# fig.show()
 
-# -------------------------------------------------------------------------------------------------------------------------
+@timer
+def DBSCAN_stage_2():
+    topography_df = pd.read_csv('topography_df.csv', index_col=0)
+    labels_matrix = []
+    for i in range(int(len(topography_df) / 360)):
+        labels_matrix.append(list(topography_df['label'][i * 360:(i + 1) * 360]))
+    labels_df = pd.DataFrame(labels_matrix)
+    dbscan = DBSCAN(eps=8, metric=dtw_metric).fit(labels_df)
+    fig = px.scatter(y=dbscan.labels_)
+    fig.show()
+    pd.DataFrame(dbscan.labels_).to_csv('dbscan_labels.csv')
 
-# PCA
 
-# pca = PCA(n_components=3)
-# pca.fit(<????????>)
-# X = pca.transform(<???????>)
-#
-# fig = px.scatter_3d(X, x=0, y=1, z=2)
-# fig.show()
-
-# -------------------------------------------------------------------------------------------------------------------------
-
-# OPTICS
-
-# optics = OPTICS(metric='euclidean').fit(<?????>)
-# reachability_ = pd.DataFrame(optics.reachability_[optics.ordering_])
-# reachability_.index = optics.ordering_
-# reachability_.to_csv('reachability_.csv')
-#
-# reachability_ = pd.read_csv('reachability_.csv')
-# fig = px.scatter(reachability_, x=reachability_.index, y='0')
-# fig.show()
-
-# -------------------------------------------------------------------------------------------------------------------------
-
-# Notes:
-
-# ...
-
-# DBSCAN da média das distâncias dos clusters formados -> clusters podem estar proximos e separados do quadrado do corredor X
-
-# Analisar o afastamento e a aproximação -> numero impar de clusters impede fazer de forma automática X
-
-# DBSCAN na lista de labels_ gerados -> os clusters variam de posição conforme o robô anda e as dimensões fixas fazem com
-# que estar próximo de uma saída a direita é um cluster e estar afastado é outro independente se o robô está entre elas,
-# o que significa estar no corredor.
+DBSCAN_stage_1()
+DBSCAN_stage_2()
