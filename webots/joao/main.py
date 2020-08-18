1275f2a212e2a71a47d28eaa925190474775fa6d
zenobio77 <zenobiojoao@gmail.com>
2020-08-18 12:13:32 -0300

    Banco de dados novo + salvado...

---
+++ webots/joao/main.py
@@ -0,0 +1,209 @@
+import math
+from functools import wraps
+from time import time
+import pandas as pd
+import numpy as np
+from sklearn.cluster import DBSCAN
+from scipy.spatial import distance
+from dtw import *
+import plotly.express as px
+from sklearn.decomposition import PCA
+
+
+def timer(function):
+    @wraps(function)
+    def _time_it(*args, **kwargs):
+        start = int(round(time() * 1000))
+        try:
+            return function(*args, **kwargs)
+        finally:
+            end_ = int(round(time() * 1000)) - start
+            name = function.__name__
+            print(f"Total execution time of {name} was: {end_ if end_ > 0 else 0} ms")
+    return _time_it
+
+
+@timer
+def get_data():
+    return pd.read_csv('leitura2.csv', index_col=0)
+
+
+@timer
+def dbscan_stage_1():
+    leitura_df = get_data()
+    x_complete = []
+    y_complete = []
+    id_complete = []
+    id = -1
+    labels_complete = []
+    for line in leitura_df.values:
+        id += 1
+        x = []
+        y = []
+        for i in range(len(line)):
+            x.append(line[i] * math.cos(math.radians(i - 90)))
+            y.append(line[i] * math.sin(math.radians(i - 90)))
+        x_y_df = pd.DataFrame(data={'x': x, 'y': y})
+        dbscan = DBSCAN(eps=3, metric='euclidean').fit(x_y_df)
+        labels_temp = [-1] * len(dbscan.labels_)
+        label_counter = 0
+        visited = 0
+        while visited < len(dbscan.labels_):
+            cluster = dbscan.labels_[np.where(dbscan.labels_ != -2)[0][0]]
+            index_list = np.where(dbscan.labels_ == cluster)[0]
+            for index in index_list:
+                if cluster != -1:
+                    labels_temp[index] = label_counter
+                dbscan.labels_[index] = -2
+                visited += 1
+            if cluster != -1:
+                label_counter += 1
+        for each in x:
+            x_complete.append(each)
+        for each in y:
+            y_complete.append(each)
+        for each in [id] * len(line):
+            id_complete.append(each)
+        for each in labels_temp:
+            labels_complete.append(each)
+    distance_complete = []
+    for reading in range(len(leitura_df)):
+        for degree in leitura_df.values[reading]:
+            distance_complete.append(degree)
+    topography_df = pd.DataFrame(data={'x': x_complete, 'y': y_complete, 'distance': distance_complete, 'id': id_complete, 'label': labels_complete})
+    topography_df.to_csv('topography_df.csv')
+
+
+@timer
+def animation_stage_1():
+    topography_df = pd.read_csv('topography_df.csv', index_col=0)
+    fig = px.scatter(topography_df, x='x', y='y', color='label', animation_frame='id', range_x=[-45, 45], range_y=[-45, 45])
+    fig.update_yaxes(dtick=1)
+    fig.update_xaxes(dtick=1)
+    fig.update_layout(height=500, width=500, title_text="Animacao dos pontos no corredor")
+    fig.show()
+
+
+class Neuron:
+    def __init__(self, receivedPattern, alphaZero, clId, distance=distance.euclidean):
+        self.centroid = receivedPattern
+        self.alphaCluster = alphaZero  # alphaCluster == Minimum similarity degree
+        self.averageRadius = -np.log(alphaZero)
+        self.distance = distance
+        # Add a cluster ID and update the total number of clusters
+        self.clusterId = clId
+
+    def activation(self, pattern):
+        return np.exp(-self.distance(self.centroid, pattern))
+
+
+    def adaptCluster(self, pattern, gamma, omega):
+        # Before updating the centroid, get the previous vector to compute p
+        previousCentroid = self.centroid.copy()
+        # Adapting centroid
+        self.centroid = self.centroid * (1 - gamma) + gamma * pattern
+        # Before updating the radius, get the previous value to compute p
+        previousAverageRadius = self.averageRadius
+        # Adapting average radius
+        self.averageRadius = (1-omega) * previousAverageRadius + omega * self.distance(pattern, previousCentroid)
+        # Calculating value of p
+        p = np.abs((self.averageRadius-previousAverageRadius) / np.maximum(self.averageRadius, previousAverageRadius))
+        # Updating the activation value for the neuron
+        self.alphaCluster = np.minimum((1 + p) * self.alphaCluster, np.exp(-self.averageRadius * (1 + p)))
+
+    def getCentroid(self):
+        return self.centroid
+
+    def getClusterId(self):
+        return self.clusterId
+
+    def getAlpha(self):
+        return self.alphaCluster
+
+    def __str__(self):
+        string = ""
+        string += "Cluster ID: " + str(self.clusterId)
+        string += "\nCentroid: " + str(self.centroid)
+        string += "\nAlpha Cluster: " + str(self.alphaCluster)
+        string += "\nAverageRadius: " + str(self.averageRadius)
+        return string
+
+
+class Sonde:
+    clustersIds = 1
+
+    def __init__(self, alphaZero=0.5, gamma=0.1, omega=0.1, distance=distance.euclidean):
+        self.alphaZero = alphaZero
+        self.gamma = gamma
+        self.omega = omega
+        self.distance = distance
+        self.listNeurons = []
+        Sonde.clustersIds = 1
+
+    def execute(self, pattern):
+        bmu = -np.inf
+        bestNeuron = None
+        for neuron in self.listNeurons:
+            # Calculating the distance and the activation function
+            # Choosing the best neuron
+            activationValue = neuron.activation(pattern)
+            if activationValue > neuron.getAlpha() and activationValue > bmu:
+                bestNeuron = neuron
+        # If we found the best neuron
+        if bestNeuron != None:
+            bestNeuron.adaptCluster(pattern, self.gamma, self.omega)
+        else:
+            bestNeuron = Neuron(pattern, self.alphaZero, Sonde.clustersIds, self.distance)
+            self.listNeurons.append(bestNeuron)
+            Sonde.clustersIds += 1
+        return bestNeuron.getClusterId()
+
+    def getListOfNeurons(self):
+        return self.listNeurons
+
+
+def dtw_metric(x, y):
+    return dtw(x, y, keep_internals=True, step_pattern="symmetric1").distance
+
+
+@timer
+def labels_df_stage_2():
+    topography_df = pd.read_csv('topography_df.csv', index_col=0)
+    labels_matrix = []
+    for i in range(int(len(topography_df) / 360)):
+        labels_matrix.append(list(topography_df['label'][i * 360:(i + 1) * 360]))
+    labels_df = pd.DataFrame(labels_matrix)
+    labels_df.to_csv('labels_df.csv')
+
+
+@timer
+def sonde_stage_3():
+    labels_df = pd.read_csv('labels_df.csv', index_col=0)
+    # Running SONDE without annimation
+    sonde = Sonde(alphaZero=0.1, gamma=0.01, omega=0.01, distance=dtw_metric)
+    listClusters = []
+    for i in range(len(labels_df)):
+      instance = labels_df.iloc[i]
+      clusterResult = sonde.execute(instance)
+      listClusters.append(clusterResult)
+    fig = px.scatter(listClusters, y=listClusters)
+    fig.show()
+
+
+def teste():
+    leitura2_df = pd.read_csv('leitura2.csv', index_col=0)
+    lista = []
+    for i in range(len(leitura2_df)):
+        lista.append(dtw(leitura2_df.iloc[0], leitura2_df.iloc[i], keep_internals=True, step_pattern="symmetric1").distance)
+    fig = px.scatter(lista, y=lista)
+    fig.show()
+    # dtw(labels_df.iloc[311], labels_df.iloc[580], keep_internals=True, step_pattern=rabinerJuangStepPattern(6, "c")).plot(type="twoway", offset=-2)
+    # print(dtw(labels_df.iloc[311], labels_df.iloc[580], keep_internals=True, step_pattern=rabinerJuangStepPattern(6, "c")).distance)
+
+
+# get_data()            #100ms
+# dbscan_stage_1()      #11909ms
+# labels_df_stage_2()
+# animation_stage_1()     #419994ms
+# SONDE_stage_3()
+teste()
